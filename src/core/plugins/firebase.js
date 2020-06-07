99cc06d820ae80f83a88dd099802f317d6f8d533
Colares <franco.colares@ues21.edu.ar>
2020-06-07 18:27:14 -0300

    Added firebase config

---
+++ src/core/plugins/firebase.js
@@ -0,0 +1,16 @@
+import * as firebase from 'firebase';
+import 'firebase/auth';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAG0xO59sbx7u6oz4jhMl5to6z6VxaanrA",
+    authDomain: "pandus-84b14.firebaseapp.com",
+    databaseURL: "https://pandus-84b14.firebaseio.com",
+    projectId: "pandus-84b14",
+    storageBucket: "pandus-84b14.appspot.com",
+    messagingSenderId: "357998900706",
+    appId: "1:357998900706:web:b8234c43815f45ca3bf5bb"
+  };
+
+firebase.initializeApp(firebaseConfig);
+
+export const auth = firebase.auth();
--- src/main.js
+++ src/main.js
@@ -1,11 +1,9 @@
 import Vue from 'vue';
-import { firestorePlugin } from 'vuefire';
 import App from './App.vue';
 import router from './core/router';
 import './core/plugins/ant-design-vue';
 
 Vue.config.productionTip = false;
-Vue.use(firestorePlugin);
 
 new Vue({
   router,
